11a10d810a18c3e61581e6e31eabad8b36c026e7
rohit-panwar <roohit.panwar@gmail.com>
2018-08-09 22:09:27 +0530

    Added day 4 code

---
+++ src/test/java/basics/Day_4.java
@@ -0,0 +1,26 @@
+package basics;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class Day_4 {
+	
+	@Test
+	public void alertTest() {
+		
+		System.setProperty("webdriver.ie.driver", "D:\\Webdriver\\drivers\\IEDriverServer.exe");
+		WebDriver driver= new InternetExplorerDriver();
+		
+		driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+		
+		driver.findElement(By.name("proceed")).click();
+		Alert a= driver.switchTo().alert();
+		a.accept();
+		//a.dismiss();
+	}
+
+	
+}
