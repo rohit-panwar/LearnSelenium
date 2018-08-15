bb63597522d3e8dabe75a5a3e954c782b35c2180
rohit-panwar <roohit.panwar@gmail.com>
2018-08-15 11:41:27 +0530

    Pushing Day 10 code to GitRepo: HTML Unit Driver/ Ghost driver

---
+++ src/test/java/basics/Day_10.java
@@ -0,0 +1,34 @@
+package basics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.annotations.Test;
+
+public class Day_10 {
+	
+	@Test
+	public void htmlUnitDriver() {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Webdriver\\drivers\\chromedriver.exe");
+
+		//WebDriver driver = new ChromeDriver();
+		WebDriver driver= new HtmlUnitDriver();
+		
+		driver.get("http://test1.absofttrainings.com");
+		System.out.println("Home Page Title:"+driver.getTitle());
+
+		driver.findElement(By.linkText("My Account")).click();
+		System.out.println("My Accout Title:"+driver.getTitle());
+		
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("testuser2");
+		driver.findElement(By.xpath("//input[contains(@name,'password')]")).sendKeys("testpwd2");
+
+		// To Click Login hyperlink
+		driver.findElement(By.name("login")).click();
+		System.out.println("After Login:"+driver.getTitle());
+	}
+	
+
+}
