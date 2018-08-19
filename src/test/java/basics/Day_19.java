b5053f0adce18b01032ac59149d926b677c7c61f
rohit-panwar <roohit.panwar@gmail.com>
2018-08-19 20:50:15 +0530

    Adding Day 19 course to Git Hub

--- src/test/java/basics/Day_19.java
+++ src/test/java/basics/Day_19.java
@@ -11,7 +11,7 @@ import org.testng.annotations.Test;
 public class Day_19 {
 
 	@Test
-	public void bootStrapDropDown() {
+	public void bootStrapDropDown1() {
 		System.setProperty("webdriver.chrome.driver", "D:\\Webdriver\\drivers\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
@@ -26,6 +26,23 @@ public class Day_19 {
 		for (int i = 0; i < list.size(); i++) {
 			list.get(i).click();
 			System.out.println(list.get(i).getText());
+		}
+	}
+	
+	@Test
+	public void bootStrapDropDown2() {
+		System.setProperty("webdriver.chrome.driver", "D:\\Webdriver\\drivers\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://v4-alpha.getbootstrap.com/components/dropdowns/");
+		
+		driver.findElement(By.id("dropdownMenuButton")).click();
+		
+		List<WebElement> list2= driver.findElements(By.xpath("//div[contains(@class,'dropdown-menu')]//a"));
+		
+		System.out.println("Total Element: "+list2.size());
+		for(int i=0; i<list2.size();i++) {
+			System.out.println(list2.get(i).getText());
 		}
 	}
 }
