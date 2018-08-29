d77d3ea0dfdde5ed93c544342ce7999938246ed3
rohit-panwar <roohit.panwar@gmail.com>
2018-08-29 22:26:35 +0530

    Pushing code to direct login through URL

---
+++ src/test/java/advanced/URL_authentication.java
@@ -0,0 +1,17 @@
+package advanced;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class URL_authentication {
+	
+	@Test
+	public void basic_auth() {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Webdriver\\drivers\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("http://admin:admin@the-internet.herokuapp.com/basic_auth");
+	}
+}
