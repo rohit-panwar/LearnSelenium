9763912f558befc6bf803e13a1e8ca30903690e0
rohit-panwar <roohit.panwar@gmail.com>
2018-09-25 22:42:10 +0530

    Adding Sikuli to git hub

--- src/test/java/sikuli/LearnSikuli.java
+++ src/test/java/sikuli/LearnSikuli.java
@@ -30,5 +30,87 @@ public class LearnSikuli {
 		s.wait(clickCreateAccount, 2000);
 		s.click();
 	}
+	
+	@Test
+	public void notePadOpen() throws FindFailed {
+		
+		Screen s= new Screen();
+		
+		s.click("notepadIcon.JPG");
+		s.find("notepad.PNG");
+		s.type("notepad.PNG", "Hello World");
+	}
+	
+	@Test
+	public void rightClick() throws FindFailed {
+		
+		Screen s= new Screen();
+		
+		s.find("notepadIcon.JPG");
+		s.rightClick("notepadIcon.JPG");
+	}
+	
+	@Test
+	public void doubleClick() throws FindFailed {
+		
+		Screen s= new Screen();
+		
+		s.click("mini.JPG");
+		s.doubleClick("edge.JPG");
+	}
+	
+	@Test
+	public void elementExist() throws FindFailed {
+		
+		Screen s= new Screen();
+		
+		s.click("mini.JPG");
+		s.exists("edge.JPG");
+	}
+	
+	@Test
+	public void wheeling() throws FindFailed {
+		
+		Screen s= new Screen();
+		
+		s.click("mini.JPG");
+		s.wheel("edge.JPG", 200, 0);
+	}
+	
+	@Test
+	public void hover() throws FindFailed {
+		
+		Screen s= new Screen();
+		
+		s.click("mini.JPG");
+		s.hover("edge.JPG");
+	}
+	
+	@Test
+	public void type() throws FindFailed {
+		
+		Screen s= new Screen();
+		
+		//s.find("cortana.JPG");
+		s.type("cortana.JPG", "NOTEPAD");
+	}
+	
+	@Test
+	public void paste() throws FindFailed {
+		
+		Screen s= new Screen();
+		
+		//s.find("cortana.JPG");
+		s.paste("cortana.JPG", "NOTEPAD");
+	}
+	
+	@Test
+	public void dragNdrop() throws FindFailed {
+		
+		Screen s= new Screen();
+		
+		s.click("mini.JPG");
+		s.dragDrop("edge.JPG", "dom.JPG");
+	}
 
 }
